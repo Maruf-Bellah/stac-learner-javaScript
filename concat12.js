a8149ffb8b2596493825a99fbb0234fa5cd71e60
marufbellah <mdmaruf5511507@gmail.com>
2023-09-07 20:39:54 +0600

    concat added

---
+++ concat12.js
@@ -0,0 +1,10 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+
+// const arr3 = arr1.concat(arr2);
+
+// const arr4 = [...arr1, ...arr2]
+
+const arr5 = [].concat(arr1, arr2, 5,9,87,98,7,87,7)
+console.log(arr5);
