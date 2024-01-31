d97c82b6abc76c8ec5a8141013d55e86271e2a13
maruf bellah <mdmaruf5511507@gmail.com>
2024-01-31 19:03:01 +0600

    data type learn from documention

---
+++ documentJsReader/javascriptTutorial/1.hello_world/2.dataType/dataType.js
@@ -0,0 +1,23 @@
+var a = new Object();
+
+// console.dir(typeof a);
+
+let s = "JavaScript";
+s[0] = "maruf";
+// console.log(s);
+
+let contact = {
+  firstName: "John",
+  lastName: "Doe",
+  email: "john.doe@example.com",
+  phone: "(408)-555-9999",
+  address: {
+    building: "4000",
+    street: "North 1st street",
+    city: "San Jose",
+    state: "CA",
+    country: "USA",
+  },
+};
+
+// console.log(contact);
