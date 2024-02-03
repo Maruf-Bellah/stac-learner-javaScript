b322364c0eef162ad689c242751c11eab9e1b321
maruf bellah <mdmaruf5511507@gmail.com>
2024-02-03 21:17:56 +0600

    something

--- arrayOfEeverything9.js
+++ arrayOfEeverything9.js
@@ -15,91 +15,84 @@ const favChannels = [
 
 // array of function
 
-
 const sum = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const times = (a, b) => a * b;
 const div = (a, b) => a / b;
 const mod = (a, b) => a % b;
 
-
-// Loop and call all functions from array 
+// Loop and call all functions from array
 const funcs = [sum, sub, times, div, mod];
 // const funcs = [sum(10,20), sub(), times(), div(), mod()];
 
 const a = 10;
 const b = 20;
 
-for(let i = 0; i < funcs.length; i++){
-    const result = funcs[i](a, b);
-    // console.log(`${funcs[i].name} Result = ${result}`);
+for (let i = 0; i < funcs.length; i++) {
+  const result = funcs[i](a, b);
+  // console.log(`${funcs[i].name} Result = ${result}`);
 }
 
 // console.log(funcs);
-
-
 
 const pointTable = [
   [0, 0],
   [3, 5],
   [5, 7],
-  [10, 23]
+  [10, 23],
 ];
 
-
-// One Dimensional Traverse 
-for(let i =0; i< pointTable.length; i++){
+// One Dimensional Traverse
+for (let i = 0; i < pointTable.length; i++) {
   // console.log(`point${i} - x=${pointTable[i]} and x=${pointTable[i][0]} and${pointTable[i][1]} total = ${pointTable[i][0] + pointTable[i][1]}`);
 }
 
-for(let i = 0; i < pointTable.length; i++){
-  for(let j =0; j < pointTable[i].length; j++){
+for (let i = 0; i < pointTable.length; i++) {
+  for (let j = 0; j < pointTable[i].length; j++) {
     // console.log(pointTable[i][j]);
   }
 }
 // console.log(pointTable);
 
+// Two Dimentonal Travese
 
-// Two Dimentonal Travese 
-
-const numbers =[
+const numbers = [
   [1, 2, 3],
   [4, 5, 6],
-  [7, 8, 9]
-]
+  [7, 8, 9],
+];
 
-for(let i =0; i <numbers.length; i++){
-  for(let j = 0; j < numbers[i].length; j++){
-      // console.log(numbers[i][j]);
-    
-    }
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers[i].length; j++) {
+    // console.log(numbers[i][j]);
+  }
 }
 
 const matrixA = [
   [0, 3],
   [2, 3],
-  [5, 8]
+  [5, 8],
 ];
 
 const matrixB = [
   [2, 0],
   [2, 6],
-  [8, 8]
+  [8, 8],
 ];
 
-
-
-const matrixSum =(matrixA, matrixB) => {
+const matrixSum = (matrixA, matrixB) => {
   const result = [];
-  for(let i = 0; i < matrixA.length; i++){
+  for (let i = 0; i < matrixA.length; i++) {
     const row = [];
-    for(let j = 0; j < matrixA[i].length; j++){
+    for (let j = 0; j < matrixA[i].length; j++) {
       // console.log(matrixA[i][j] + matrixB[i][j])
-      row.push(matrixA[i][j] + matrixB[i][j])
+      row.push(matrixA[i][j] + matrixB[i][j]);
     }
-    result.push(row)
+    result.push(row);
   }
   return result;
-}
+};
 
 matrixSum(matrixA, matrixB);
+
+console.log("maruf you have to learn js properly");
